662e1d1f3eaab2a41bce1933cf264fdad0f7f61a
Rene Cunningham <rene@compounddata.com>
2024-11-28 07:24:11 +1100

    refactor update sub command

--- internal/cli/account.go
+++ internal/cli/account.go
@@ -3,21 +3,16 @@ package cli
 import (
 	"context"
 	"database/sql"
-	"encoding/json"
-	"errors"
 	"fmt"
+	"gt/internal/marshal"
 	"gt/models/gnucash"
 	"strings"
 
 	_ "github.com/mattn/go-sqlite3"
 	"github.com/spf13/cobra"
-	"github.com/volatiletech/sqlboiler/boil"
+	"github.com/volatiletech/null/v8"
+	"github.com/volatiletech/sqlboiler/v4/boil"
 	"github.com/volatiletech/sqlboiler/v4/queries/qm"
-)
-
-var (
-	GnuCashSqliteURI   = "/home/rene/.gnucash.sql.gnucash?mode=ro&_query_only=true"
-	GnuCashSqliteURIRw = "/home/rene/.gnucash.sql.gnucash"
 )
 
 func accountCmd(cli *cli) *cobra.Command {
@@ -26,31 +21,14 @@ func accountCmd(cli *cli) *cobra.Command {
 	}
 	cmd.AddCommand(getAccountCmd(cli))
 	cmd.AddCommand(listAccountCmd(cli))
+	cmd.AddCommand(updateAccountCmd(cli))
 	return cmd
 }
 
-// getAccountFromGUIDOrAccountTree accepts a string which is an account GUID or
-// a case insensitive absolute account name in gnucash syntax and returns the
-// account.
-func getAccountFromGUIDOrAccountTree(ctx context.Context, db *sql.DB, s string) (*gnucash.Account, error) {
-	var account *gnucash.Account
-	var err error
-	account, err = gnucash.Accounts(qm.Where("guid=?", s)).One(ctx, db)
-	if err != nil {
-		switch {
-		case errors.Is(err, sql.ErrNoRows):
-			return getAccountTree(ctx, db, s)
-		default:
-			return account, err
-		}
-	}
-	return account, nil
-}
-
-// getAccountTree accepts a string which represents a case insensitive absolute
+// getAccountFromAccountTreeString accepts a string which represents a case insensitive absolute
 // account name in gnucash syntax (e.g. expenses:automotive:petrol) and returns
 // the final child account.
-func getAccountTree(ctx context.Context, db *sql.DB, s string) (*gnucash.Account, error) {
+func getAccountFromAccountTreeString(ctx context.Context, db *sql.DB, s string) (*gnucash.Account, error) {
 	var account *gnucash.Account
 	var err error
 	accounts := strings.Split(s, ":")
@@ -78,6 +56,88 @@ func getAccountTree(ctx context.Context, db *sql.DB, s string) (*gnucash.Account
 	return account, nil
 }
 
+func updateAccountCmd(cli *cli) *cobra.Command {
+	var flags struct {
+		ParentAccount string
+		Name          string
+		Description   string
+	}
+	var cmd = &cobra.Command{
+		Use: "update",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			if len(args) != 1 {
+				return ErrAccountMissing
+			}
+			guidOrAccountName := args[0]
+
+			account, err := cli.getAccountFromGUIDOrAccountTree(cmd.Context(), guidOrAccountName)
+			if err != nil {
+				return ErrAccountDoesNotExist
+			}
+
+			columns := []string{}
+
+			parentAccount := &gnucash.Account{}
+			if flags.ParentAccount != "" {
+				parentAccount, err = cli.getAccountFromGUIDOrAccountTree(cmd.Context(), flags.ParentAccount)
+				if err != nil {
+					return err
+				}
+
+				if parentAccount.GUID != "" {
+					account.ParentGUID = null.StringFrom(parentAccount.GUID)
+					columns = append(columns, "parent_guid")
+				}
+			}
+
+			// TODO(rene): must check that name is a valid is name a valid
+			// child account name (i.e. does not contain ":").
+			if flags.Name != "" {
+				account.Name = flags.Name
+				exists, err := cli.accountExists(cmd.Context(), account)
+				if err != nil {
+					return err
+				}
+				if exists {
+					return ErrAccountAlreadyExists
+				}
+				columns = append(columns, "name")
+			}
+
+			if flags.Description != "" {
+				account.Description = null.StringFrom(flags.Description)
+				columns = append(columns, "description")
+			}
+
+			tx, err := cli.db.BeginTx(cmd.Context(), nil)
+			if err != nil {
+				return err
+			}
+			defer tx.Rollback()
+
+			if _, err := account.Update(cmd.Context(), tx, boil.Whitelist(columns...)); err != nil {
+				return err
+			}
+
+			if err := tx.Commit(); err != nil {
+				return err
+			}
+
+			resp, err := marshal.NewAccountMarshal(account).JSON()
+			if err != nil {
+				return err
+			}
+			fmt.Fprintf(cmd.OutOrStdout(), string(resp))
+
+			return nil
+		},
+	}
+	cmd.Flags().StringVar(&flags.ParentAccount, "parent-account", "", "Parent Account")
+	cmd.Flags().StringVar(&flags.Name, "name", "", "Account Name")
+	cmd.Flags().StringVar(&flags.Description, "description", "", "Account Description")
+	return cmd
+}
+
 func getAccountCmd(cli *cli) *cobra.Command {
 	var cmd = &cobra.Command{
 		Use: "get",
@@ -87,30 +147,16 @@ func getAccountCmd(cli *cli) *cobra.Command {
 			}
 			guidOrAccountName := args[0]
 
-			db, err := sql.Open("sqlite3", cli.config.GnucashDBFile)
+			account, err := cli.getAccountFromGUIDOrAccountTree(cmd.Context(), guidOrAccountName)
 			if err != nil {
 				return err
 			}
-			boil.SetDB(db)
 
-			account, err := gnucash.Accounts(qm.Where("guid=?", guidOrAccountName)).One(cmd.Context(), db)
-			if err != nil {
-				switch {
-				case errors.Is(err, sql.ErrNoRows):
-					account, err = getAccountTree(cmd.Context(), db, guidOrAccountName)
-					if err != nil {
-						return err
-					}
-				default:
-					return err
-				}
-			}
-
-			resp, err := json.MarshalIndent(account, "", "  ")
+			resp, err := marshal.NewAccountMarshal(account).JSON()
 			if err != nil {
 				return err
 			}
-			fmt.Println(string(resp))
+			fmt.Fprintf(cmd.OutOrStdout(), string(resp))
 			return nil
 		},
 	}
@@ -126,26 +172,20 @@ func listAccountCmd(cli *cli) *cobra.Command {
 		RunE: func(cmd *cobra.Command, args []string) error {
 			var q []qm.QueryMod
 
-			db, err := sql.Open("sqlite3", cli.config.GnucashDBFile)
-			if err != nil {
-				return err
-			}
-			boil.SetDB(db)
-
 			if flags.NameLike != "" {
 				q = append(q, qm.Where("name LIKE ?", flags.NameLike))
 			}
 
-			accounts, err := gnucash.Accounts(q...).All(cmd.Context(), db)
+			accounts, err := gnucash.Accounts(q...).All(cmd.Context(), cli.db)
 			if err != nil {
 				return err
 			}
 
-			resp, err := json.MarshalIndent(accounts, "", "  ")
+			resp, err := marshal.NewAccountsMarshal(accounts).JSON()
 			if err != nil {
 				return err
 			}
-			fmt.Println(string(resp))
+			fmt.Fprintf(cmd.OutOrStdout(), string(resp))
 			return nil
 		},
 	}
--- internal/cli/cli.go
+++ internal/cli/cli.go
@@ -1,14 +1,28 @@
 package cli
 
 import (
+	"context"
 	"database/sql"
 	"encoding/json"
+	"errors"
+	"fmt"
+	"gt/models/gnucash"
 	"os"
 	"path"
+	"slices"
+	"strings"
 	"sync"
 
 	"github.com/spf13/cobra"
 	"github.com/volatiletech/sqlboiler/v4/boil"
+	"github.com/volatiletech/sqlboiler/v4/queries/qm"
+)
+
+var (
+	ErrAccountDoesNotExist  = errors.New("account does not exist")
+	ErrAccountMissingParent = errors.New("account missing parent")
+	ErrAccountMissing       = errors.New("account name or guid missing")
+	ErrAccountAlreadyExists = errors.New("account already exists")
 )
 
 type cli struct {
@@ -65,4 +79,58 @@ func (c *cli) initContext() error {
 	boil.SetDB(c.db)
 
 	return nil
+}
+
+// accountExists accepts an account and checks if it exists.
+func (c *cli) accountExists(ctx context.Context, account *gnucash.Account) (bool, error) {
+	p, err := gnucash.Accounts(qm.Where("guid=?", account.ParentGUID.String)).One(ctx, c.db)
+	if err != nil {
+		return false, err
+	}
+	return gnucash.Accounts(qm.Where("name=? AND parent_guid=?", account.Name, p.GUID)).Exists(ctx, c.db)
+}
+
+// getAccountFromGUIDOrAccountTree accepts a string which is an account GUID or
+// a case insensitive absolute account name in gnucash syntax and returns the
+// account.
+func (c *cli) getAccountFromGUIDOrAccountTree(ctx context.Context, s string) (*gnucash.Account, error) {
+	var account *gnucash.Account
+	var err error
+	account, err = gnucash.Accounts(qm.Where("guid=?", s)).One(ctx, c.db)
+	if err != nil {
+		switch {
+		case errors.Is(err, sql.ErrNoRows):
+			return getAccountFromAccountTreeString(ctx, c.db, s)
+		default:
+			return account, err
+		}
+	}
+	return account, nil
+}
+
+func (c *cli) getAccountTreeString(ctx context.Context, accountTree []*gnucash.Account) (s string, err error) {
+	var b strings.Builder
+	for _, account := range accountTree {
+		b.WriteString(fmt.Sprintf("%s:", account.Name))
+	}
+	return b.String(), nil
+}
+
+// getAccountTree returns a slice containing account tree for a gnucash.Account.
+// Slice is ordered with child from Root account being the first item.
+func (c *cli) getAccountTree(ctx context.Context, account *gnucash.Account) ([]*gnucash.Account, error) {
+	accountTree := []*gnucash.Account{account}
+	for {
+		parentAccount, err := gnucash.Accounts(qm.Where("guid=?", account.ParentGUID.String)).One(ctx, c.db)
+		if err != nil {
+			return accountTree, err
+		}
+		accountTree = append(accountTree, parentAccount)
+		account = parentAccount
+		if parentAccount.ParentGUID.String == "" {
+			break
+		}
+	}
+	slices.Reverse(accountTree)
+	return accountTree, nil
 }
